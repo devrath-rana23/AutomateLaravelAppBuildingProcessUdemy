cdeedc7e5309ae8b2b4c8bd71f7225511afffa3c
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-07 23:41:13 +0530

    generate employee controller

--- emp_sys/blueprint.sh
+++ emp_sys/blueprint.sh
@@ -2,3 +2,7 @@ php artisan blueprint:erase
 php artisan blueprint:build
 php artisan migrate:fresh
 php artisan db:seed
+php artisan db:seed --class="ProjectSeeder"
+php artisan db:seed --class="EmployeeSeeder"
+php artisan db:seed --class="DepartmentSeeder"
+php artisan db:seed --class="ContactInfoSeeder"
