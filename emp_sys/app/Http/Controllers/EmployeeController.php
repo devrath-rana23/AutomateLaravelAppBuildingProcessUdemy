e9684c567560aab07b0c530eb84236cac3722ef1
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-08 00:45:11 +0530

    make send mail

--- emp_sys/app/Http/Controllers/EmployeeController.php
+++ emp_sys/app/Http/Controllers/EmployeeController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ThanksMail;
 use App\Models\Employee;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 
 class EmployeeController extends Controller
 {
@@ -17,6 +15,6 @@ class EmployeeController extends Controller
     {
         $employee = Employee::find($id);
 
-        Mail::to($employee->contactInfo->email)->send(new ThanksMail());
+        $request->session()->store('employee.name', $employee->name);
     }
 }
--- emp_sys/tests/Feature/Http/Controllers/EmployeeControllerTest.php
+++ emp_sys/tests/Feature/Http/Controllers/EmployeeControllerTest.php
@@ -2,10 +2,8 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use App\Mail\ThanksMail;
 use App\Models\Employee;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 /**
@@ -22,12 +20,8 @@ class EmployeeControllerTest extends TestCase
     {
         $employee = Employee::factory()->create();
 
-        Mail::fake();
-
         $response = $this->get(route('employee.test'));
 
-        Mail::assertSent(ThanksMail::class, function ($mail) {
-            return $mail->hasTo($employee->contactInfo->email);
-        });
+        $response->assertSessionHas('employee.name', $employee->name);
     }
 }
